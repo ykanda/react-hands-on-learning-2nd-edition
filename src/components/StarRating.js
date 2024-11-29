b76d9d306169e5c5fd1723d9ae0da2bf1f9aa603
kandayasu <322414+ykanda@users.noreply.github.com>
2024-11-29 13:03:27 +0000

    Update

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 import Star from "./Star";
 
-export default function StarRating({ totalStars = 5 }) {
-    const [ selectedStars, setSelectedStars ] = useState(3);
+// style, totalStars を除いたプロパティ（残余プロパティ）を props として受け取っている
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
+    const [ selectedStars, setSelectedStars ] = useState(0);
 
-    // Star がクリックされたると setSelectedStars onSelect に渡してた関数が呼び出される
-    const s = [ ...Array(totalStars) ]
+    // Star がクリックされると setSelectedStars onSelect に渡してた関数が呼び出される
+    // props はスプレッド構文で展開して div にわたされている
     return (
-        <>
-            {s.map((n, i) => (<Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />))}
+        <div style={{ padding: "5px", ...style }} { ...props }>
+            {[ ...Array(totalStars) ].map((n, i) => (
+                <Star
+                    key={i}
+                    selected={selectedStars > i}
+                    onSelect={() => setSelectedStars(i + 1)}
+                />))}
             <p>
                 {selectedStars} of {totalStars} stars
             </p>
-        </>
+        </div>
     );
 }
